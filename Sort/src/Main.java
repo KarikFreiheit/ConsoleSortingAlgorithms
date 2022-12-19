c36508a9ad2d4f652dfc52bc9f8468af91e60f26
KarikFreiheit <crossoverkarik@gmail.com>
2022-12-19 12:20:12 -0800

    Selection Sort and Imperfect Array Generation added.

--- Sort/src/Main.java
+++ Sort/src/Main.java
@@ -2,16 +2,17 @@ import java.awt.*;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
+import static java.lang.Integer.MAX_VALUE;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
 
-        Integer[] values = GeneratePerfectArray(15);
-        DrawArray(values);
-        bubbleSort(values);
+        Integer[] values = generateImperfectArray(15);
+        drawArray(values);
+        selectionSort(values);
 
 
 
@@ -20,7 +21,7 @@ public class Main {
     //ALGORITHMS=======================================================================
 
     //Bubble Sort
-    public static void bubbleSort(Integer[] array){
+    public static void bubbleSort(Integer[] array) throws InterruptedException {
         for(int i = 0; i < array.length; i++){
             for(int j = 0; j < array.length-i-1; j++){
                 int a = array[j];
@@ -28,12 +29,60 @@ public class Main {
                 if(a>b) {
                     swap(array, j, j + 1);
                 }
-                //DrawArray(array); - Will output each step of the process
+                 // Will output each step of the process
+                drawArray(array);
             }
         }
         //Outputs only the finished array
-        DrawArray(array);
+        drawArray(array);
     }
+
+    //Insertion Sort
+
+    public static void insertionSort(Integer[] array){
+        int n = array.length;
+        for(int i = 1; i < n; ++i){
+
+                int k = array[i];
+                int j = i - 1;
+
+                while( j >= 0 && array[j] > k){
+                    array[j + 1] = array[j];
+                    j -= 1;
+                }
+                array[j + 1] = k;
+            // Will output each step of the process
+            drawArray(array);
+
+        }
+        drawArray(array);
+
+
+    }
+
+    //Selection Sort
+    public static void selectionSort(Integer[] array){
+
+        int j = array.length;
+        for(int i = 0; i < j - 1; i++){
+            int index = i;
+            for(int k = i+1; k < j; k++){
+                if(array[k] < array[index])
+                {
+                    index = k;
+                }
+
+            }
+
+            int temp = array[index];
+            array[index] = array[i];
+            array[i] = temp;
+        }
+        drawArray(array);
+
+    }
+
+
 
     //Other=======================================================================
 
@@ -42,9 +91,9 @@ public class Main {
         array[i] = array[j];
         array[j] = temp;
     }
+
     //Generates an array with no repeats
-    //Generates an array with no repeats
-    public static Integer[] GeneratePerfectArray(int n){
+    public static Integer[] generatePerfectArray(int n){
         Integer[] array = new Integer[n];
         for (int i = 0; i < array.length; i++){
             array[i] = i + 1;
@@ -52,11 +101,19 @@ public class Main {
         Collections.shuffle(Arrays.asList(array));
         return array;
     }
+    public static Integer[] generateImperfectArray(int n){
+        Integer[] array = new Integer[n];
+        Random rand = new Random();
+        for (int i = 0; i < array.length; i++){
+            array[i] = rand.nextInt(0, n + 1);
+        }
+        return array;
+    }
 
     //Drawing=======================================================================
 
     //Outputs Array as a visual from the left side of console to the right
-    public static void DrawWallArray(Integer[] array){
+    public static void drawWallArray(Integer[] array){
 
         for(int i = 0; i < array.length; i++){
             int height  = array[i];
@@ -68,7 +125,7 @@ public class Main {
     }
 
     //Outputs Array as a visual from the bottom of console to the top
-    public static void DrawArray(Integer[] array){
+    public static void drawArray(Integer[] array){
         for(int i = 0; i < array.length; i++) {
             for (int j = array.length; j > 0; j--) {
                 if (array[j - 1] <= i + 1) {
